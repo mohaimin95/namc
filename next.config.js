714a609c468aaaba692f62fa76f86d6640c73881
Abdul Mohaimin <32485005+mohaimin95@users.noreply.github.com>
2021-02-13 10:53:32 +0530

    added loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 module.exports = {
-  assetPrefix: ghPages ? '/dotcs.github.io/' : ''   // customize this value
+  assetPrefix: ghPages ? '/namc.github.io/' : ''   // customize this value,
+  images: {
+    loader: "imgix",
+  }
 };
